a5956715fe979b61100e5b300664ea6ee18f35c7
avneetahuja <aahuja16@asu.edu>
2024-06-01 11:08:47 -0700

    added body visibility checks to eliminate arbitrary rep count raises

--- app/components/PoseEstimation.tsx
+++ app/components/PoseEstimation.tsx
@@ -35,6 +35,7 @@ const PoseEstimation: React.FC = () => {
     const [right_shoulder_raise_stage, setRightShoulderRaiseStage] = React.useState<string>("down");
     const [left_shoulder_raise_count, setLeftShoulderRaiseCount] = React.useState<number>(0);
     const [right_shoulder_raise_count, setRightShoulderRaiseCount] = React.useState<number>(0);
+    const [bodyVisible, setBodyVisible] = React.useState<boolean>(true);
 
   useEffect(() => {
     const pose = new Pose({
@@ -123,62 +124,102 @@ const PoseEstimation: React.FC = () => {
         const left_hip = results.poseLandmarks[23];
         const right_hip = results.poseLandmarks[24];
 
-        setLeftCurlAngle(calculateAngle(left_shoulder, left_elbow, left_wrist));
-        setRightCurlAngle(
-          calculateAngle(right_shoulder, right_elbow, right_wrist)
-        );
-        setLeftLateralRaiseAngle(
-          calculateAngle(left_hip, left_shoulder, left_elbow)
-        );
-        setRightLateralRaiseAngle(
-          calculateAngle(right_hip, right_shoulder, right_elbow)
-        );
+        if (
+            left_shoulder &&
+            left_elbow &&
+            left_wrist &&
+            right_shoulder &&
+            right_elbow &&
+            right_wrist &&
+            left_hip &&
+            right_hip &&
+            left_shoulder.visibility > 0.5 &&
+            left_elbow.visibility > 0.5 &&
+            left_wrist.visibility > 0.5 &&
+            right_shoulder.visibility > 0.5 &&
+            right_elbow.visibility > 0.5 &&
+            right_wrist.visibility > 0.5 &&
+            left_hip.visibility > 0.5 &&
+            right_hip.visibility > 0.5
+          )
+          {
+            setLeftCurlAngle(calculateAngle(left_shoulder, left_elbow, left_wrist));
+            setRightCurlAngle(
+              calculateAngle(right_shoulder, right_elbow, right_wrist)
+            );
+            setLeftLateralRaiseAngle(
+              calculateAngle(left_hip, left_shoulder, left_elbow)
+            );
+            setRightLateralRaiseAngle(
+              calculateAngle(right_hip, right_shoulder, right_elbow)
+            );
+            setBodyVisible(true);
+          }
+          else{
+            setLeftCurlAngle(0);
+            setRightCurlAngle(0);
+            setLeftLateralRaiseAngle(0);
+            setRightLateralRaiseAngle(0);
+            setLeftCurlStage("down");
+            setRightCurlStage("down");
+            setLeftLateralRaiseStage("down");
+            setRightLateralRaiseStage("down");
+            setLeftShoulderRaiseStage("down");
+            setRightShoulderRaiseStage("down");
+            setBodyVisible(false);
+          }
+        
+      }
+      else{
+        setBodyVisible(false);
       }
       canvasCtx.restore();
     }
   };
 
   useEffect(() => {
-    if (left_curl_angle > 170) {
+    if (bodyVisible && left_curl_angle > 170) {
       setLeftCurlStage("down");
     }
   }, [left_curl_angle]);
   useEffect(() => {
-    if (left_curl_angle < 90 && left_curl_stage === "down") {
+    if (bodyVisible && left_curl_angle < 90 && left_curl_stage === "down") {
       setLeftCurlStage("up");
       setLeftCurlCount(left_curl_count + 1);
     }
   }, [left_curl_angle]);
   useEffect(() => {
-    if (right_curl_angle > 170) {
+    if (bodyVisible && right_curl_angle > 170) {
       setRightCurlStage("down");
     }
   }, [right_curl_angle]);
   useEffect(() => {
-    if (right_curl_angle < 90 && right_curl_stage === "down") {
+    if (bodyVisible && right_curl_angle < 90 && right_curl_stage === "down") {
       setRightCurlStage("up");
       setRightCurlCount(right_curl_count + 1);
     }
   }, [right_curl_angle]);
   useEffect(() => {
-    if (left_lateral_raise_angle < 20) {
+    if (bodyVisible && left_lateral_raise_angle < 20) {
       setLeftLateralRaiseStage("down");
     }
   }, [left_lateral_raise_angle]);
   useEffect(() => {
-    if (left_lateral_raise_angle > 75 && left_lateral_raise_stage === "down") {
+    if (bodyVisible && left_lateral_raise_angle > 65 && left_lateral_raise_stage === "down") {
+        
       setLeftLateralRaiseStage("up");
       setLeftLateralRaiseCount(left_lateral_raise_count + 1);
     }
   }, [left_lateral_raise_angle]);
   useEffect(() => {
-    if (right_lateral_raise_angle < 20) {
+    if (bodyVisible && right_lateral_raise_angle < 20) {
       setRightLateralRaiseStage("down");
     }
   }, [right_lateral_raise_angle]);
   useEffect(() => {
     if (
-      right_lateral_raise_angle > 75 &&
+        bodyVisible &&
+      right_lateral_raise_angle > 65 &&
       right_lateral_raise_stage === "down"
     ) {
       setRightLateralRaiseStage("up");
@@ -186,18 +227,18 @@ const PoseEstimation: React.FC = () => {
     }
   }, [right_lateral_raise_angle]);
   useEffect(() => {
-    if (left_curl_angle>160 && left_lateral_raise_angle>160 && left_shoulder_raise_stage==="down"){
+    if (bodyVisible && left_curl_angle>160 && left_lateral_raise_angle>160 && left_shoulder_raise_stage==="down"){
       setLeftShoulderRaiseStage("up");
       setLeftShoulderRaiseCount(left_shoulder_raise_count+1)
     }
-    else if(left_curl_angle<160 || left_lateral_raise_angle<160){
+    else if(bodyVisible && left_curl_angle<160 || left_lateral_raise_angle<160){
         setLeftShoulderRaiseStage("down");
     }
-    if(right_curl_angle>160 && right_lateral_raise_angle>160 && right_shoulder_raise_stage==="down"){
+    if(bodyVisible && right_curl_angle>160 && right_lateral_raise_angle>160 && right_shoulder_raise_stage==="down"){
       setRightShoulderRaiseStage("up");
       setRightShoulderRaiseCount(right_shoulder_raise_count+1)
     }
-    else if(right_curl_angle<160 || right_lateral_raise_angle<160){
+    else if(bodyVisible && right_curl_angle<160 || right_lateral_raise_angle<160){
         setRightShoulderRaiseStage("down");
     }
   },[left_curl_angle, right_curl_angle, left_lateral_raise_angle, right_lateral_raise_angle]);
@@ -216,7 +257,9 @@ const PoseEstimation: React.FC = () => {
       <video ref={video_ref} style={{ display: "none" }}></video>
       <canvas ref={canvas_ref} width={1280} height={720}></canvas>
       <div>
-        <h1>Left Angle: {left_curl_angle.toFixed(2)}</h1>
+        {bodyVisible ? (
+            <div>
+                <h1>Left Angle: {left_curl_angle.toFixed(2)}</h1>
         <h1>Right Angle: {right_curl_angle.toFixed(2)}</h1>
         <h1>Left Count: {left_curl_count}</h1>
         <h1>Right Count: {right_curl_count}</h1>
@@ -238,6 +281,12 @@ const PoseEstimation: React.FC = () => {
         <button className="bg-red-500 p-8" onClick={handleReset}>
           Reset
         </button>
+                </div> ) : (
+            <div>
+                <h1>Body Not Visible</h1>
+                </div>
+                )}
+        
       </div>
     </div>
   );
